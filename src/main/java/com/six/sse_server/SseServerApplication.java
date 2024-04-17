18d4beadcd9f2ffe27b0cce5b5b29626fcd41fc5
si eun <tldms123455@naver.com>
2024-04-17 18:35:04 +0900

    :tada: init commit

---
+++ src/main/java/com/six/sse_server/SseServerApplication.java
@@ -0,0 +1,13 @@
+package com.six.sse_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseServerApplication.class, args);
+    }
+
+}
