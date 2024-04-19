b0ddfb43f94adb01d80d253748cb81826820c212
kudongku <kudongku@naver.com>
2024-04-19 15:04:31 +0900

    :sparkles: feat(global) : url origin 환경변수로 설정

--- src/main/java/com/six/sse_server/config/WebSecurityConfig.java
+++ src/main/java/com/six/sse_server/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.six.sse_server.config;
 
 import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,19 +11,19 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class WebSecurityConfig {
 
+    @Value("${spring.main.url}")
+    private String mainUrl;
+    @Value("${spring.sse.url}")
+    private String sseUrl;
+    @Value("${spring.front.url}")
+    private String frontUrl;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(
-            Arrays.asList(
-                "http://localhost:8080",
-                "http://localhost:5173",
-                "http://localhost:8081"
-            )
-        );
+        configuration.setAllowedOrigins(Arrays.asList(mainUrl,frontUrl, sseUrl));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(
-            Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
