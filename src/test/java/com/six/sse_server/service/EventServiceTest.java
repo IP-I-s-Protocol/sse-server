8118f04cf669d49b5974e2a270016e1c2241ea89
si eun <tldms123455@naver.com>
2024-04-29 19:08:33 +0900

    :test_tube: test(global) : 이벤트 구독 실패 테스트 추가

--- src/test/java/com/six/sse_server/service/EventServiceTest.java
+++ src/test/java/com/six/sse_server/service/EventServiceTest.java
@@ -1,14 +1,12 @@
 package com.six.sse_server.service;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.six.sse_server.values.EventServiceTestValues;
 import java.io.IOException;
@@ -16,9 +14,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.connection.DefaultMessage;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
@@ -33,6 +35,8 @@ class EventServiceTest implements EventServiceTestValues {
     @Mock
     private RedisMessageListenerContainer redisContainer;
 
+    @Mock
+    private SseEmitter sseEmitter;
 
     @Nested
     @DisplayName("옥션 이벤트 구독 테스트")
@@ -47,12 +51,29 @@ class EventServiceTest implements EventServiceTestValues {
                 .addMessageListener(any(MessageListenerAdapter.class), any(ChannelTopic.class));
         }
 
-//        @Test
-//        void 옥션_이벤트_구독_실패_테스트() throws IOException {
-//            SseEmitter sseEmitter = mock();
-//            doThrow(IOException.class).when(sseEmitter).send(SseEmitter.event().data(anyString()));
-//            given(sseEmitter.send(SseEmitter.event().data(anyString()))).willThrow(IOException.class);
-//            eventService.subscribeAuctionEvent(TEST_AUCTION1_ID, sseEmitter);
-//        }
+        @Test
+        void 옥션_이벤트_구독_실패_테스트() throws IOException {
+            //given
+            ChannelTopic topic = new ChannelTopic("auction-price:" + TEST_AUCTION1_ID);
+
+            doThrow(IOException.class).when(sseEmitter).send(anyString());
+            //when
+            eventService.subscribeAuctionEvent(TEST_AUCTION1_ID, sseEmitter);
+
+            ArgumentCaptor<MessageListener> listenerCaptor = ArgumentCaptor.forClass(MessageListener.class);
+            verify(redisContainer).addMessageListener(listenerCaptor.capture(), eq(topic));
+
+            MessageListener listener = listenerCaptor.getValue();
+            byte[] messageBody = "Test message".getBytes();
+            Message message = new DefaultMessage(topic.getTopic().getBytes(), messageBody);
+            //then
+            try {
+                listener.onMessage(message, topic.getTopic().getBytes());
+            } catch (Exception e) {
+                verify(sseEmitter).completeWithError(any(IOException.class));
+            }
+        }
+
+
     }
 }
